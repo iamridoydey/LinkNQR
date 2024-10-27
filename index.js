6d2d73a2ff5d9bdd26dfd9df88280d58c53fa7c5
Ridoy Chandra Dey <iamridoydey@gmail.com>
2024-10-28 00:53:27 +0600

    Error fixed for middlewares express.json-> express.json()

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"))
 
 // Middlewares
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, () => {
