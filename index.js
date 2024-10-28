6541bb584e552d03253619c66ae702cb24764ffb
Ridoy Chandra Dey <iamridoydey@gmail.com>
2024-10-28 17:15:13 +0600

    Install dotenv and added configuration to the index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const connectMongoDB = require("./connect");
 const path = require("path");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 const staticRoute = require("./routes/static.route");
 const userRoute = require("./routes/user.route");
 const urlRoute = require("./routes/url.route");
@@ -18,20 +19,19 @@ const app = express();
 
 // Set Up views (ejs)
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
-app.set("views", path.resolve("./views"))
+app.set("views", path.resolve("./views"));
 
 // Middlewares
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Middlewares with route
-app.use("/", staticRoute)
-app.use("/", userRoute)
-app.use("/", validUser, urlRoute)
-
+app.use("/", staticRoute);
+app.use("/", userRoute);
+app.use("/", validUser, urlRoute);
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
